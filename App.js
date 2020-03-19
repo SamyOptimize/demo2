1e9ac02bbb08975fc6b24082fad652ecb6b331a1
SamyOptimize <samy@optimizekw.com>
2020-03-19 16:45:21 +0300

    First init

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Updates } from "expo";
+
+export default class App extends Component {
+  state = { error: "", status: "" };
+  handleUpdates = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        this.setState({
+          status: "New update found"
+        });
+        await Updates.fetchUpdateAsync();
+        Updates.reloadFromCache();
+      }
+    } catch (e) {
+      console.log(e);
+
+      this.setState({ error: JSON.stringify(e) });
+    }
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="Check update" onPress={this.handleUpdates} />
+        <Text selectable>Status:{this.state.status}</Text>
+        <Text selectable>Error:{this.state.error}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp12907c2957a0460598a747a888efa503://192.168.43.55:19004";
+
+}
